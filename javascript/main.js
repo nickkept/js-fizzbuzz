e439d8a7181a3d02ebefd68a37ec39e47cdd0f52
nickkept <nickit1899@gmail.com>
2022-11-02 15:58:12 +0100

    finished project without bonus

--- javascript/main.js
+++ javascript/main.js
@@ -1,20 +1,23 @@
 for (let i = 0; i <= 100; i++) {
     const multiplo3 = i % 3 === 0;
     const multiplo5 = i % 5 === 0;
-    const multiplo35 = i % 3 ===0 && i % 5 === 0
-if(multiplo3){
-        i = `${i} frizz`;
-        console.log(i);
-    }
-    
-if(multiplo5){
-        i = `${i} buzz`;
-        console.log(i);
-    }
-if(multiplo35){
-        i = `${i} fizzbuzz`;
-        console.log(i);
-    }
+    const multiplo15 = i % 3 === 0 && i % 5 === 0;
 
+    // i = 15
 
+    if (multiplo15) {
+
+        console.log(`fizzbuzz`);
+    }
+    else if (multiplo5) {
+
+        console.log(`buzz`);
+    }
+    else if (multiplo3) {
+
+        console.log(`fizz`);
+    }
+    else {
+        console.log(i);
+    }
 }
